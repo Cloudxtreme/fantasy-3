777452938a3b241c8f71f36e15722446dc7e134b
LaurynasMiksys <lmiksys@gmail.com>
2015-09-02 00:03:47 +0300

    Reading data. Putting in Database object in raw format.

--- premier-league/get_data.py
+++ premier-league/get_data.py
@@ -1,5 +1,10 @@
+import time
 import urllib2
-from HTMLParser import HTMLParser
+from itertools import product
+
+from pylearn2.utils import serial
+
+
 
 
 element_filter = {'te_1': 'Arsenal',
@@ -49,11 +54,11 @@ stat_filter = {'total_points': 'Total score',
                'transfers_in': 'Transfers in',
                'transfers_out': 'Transfers out',
                'transfers_in_event': 'Transfers in (round)',
-               'transfers_out_event': 'Transfers out (round)',
-               'cost_change_start': 'Price rise',
-               'cost_change_start_fall': 'Price fall',
-               'cost_change_event': 'Price rise (round)',
-               'cost_change_event_fall': 'Price fall (round)',}
+               'transfers_out_event': 'Transfers out (round)',}
+#                'cost_change_start': 'Price rise',
+#                'cost_change_start_fall': 'Price fall',
+#                'cost_change_event': 'Price rise (round)',
+#                'cost_change_event_fall': 'Price fall (round)',}
 
 
 class Data(object):
@@ -61,20 +66,31 @@ class Data(object):
     def __init__(self):
         self.data = dict()
         
-    def add_data_block(self, header, content):
+    def add_data_block(self, playerID, header, content):
 #         assert len(header)==(len(content)-1)
-        playerID = content[0]
-        oldState = self.data[playerID]
-        newInfo = {h: c for h, c in zip(header, content[1:])}
-        newState = oldState.update(newInfo)
-        self.data[playerID] = newState
+        state = self.data[playerID] if self.data.has_key(playerID) else {}
+        newInfo = {h: c for h, c in zip(header, content)}
+        state.update(newInfo)
+        self.data[playerID] = state
     
 
 
 def ParseContentBlock(block):
     
+    block = [line.strip() for line in block[1:]]
     
-    return block
+    playerID = block[0].split(' ')
+    if len(playerID)>1:
+        playerID = int(playerID[1].split('"')[1][1:])
+    else:
+        playerID = 'ID'
+    
+    block = [line.strip('</th>') for line in block[1:]]
+    block = [line.strip('</td>') for line in block]
+    block = [line if not line[:4]=='abbr' else line.split('"')[1] for line in block]
+    block = [line for line in block if not line=='']
+    
+    return playerID, block
 
 
 
@@ -85,11 +101,11 @@ if __name__=="__main__":
     urlpath = 'http://fantasy.premierleague.com/stats/elements/'
     
     ef = 'te_1'
-    sf = 'total_points' 
+    sf = 'assists' 
     
-    parser = HTMLParser()
-    
-    for page in range(2):
+    """
+    t = time.time()    
+    for ef, sf, page in product(element_filter, stat_filter, range(2)):
         _urlpath = urlpath+'?element_filter='+ef+'&stat_filter='+sf+'&page='+str(page+1)
         f = urllib2.urlopen(_urlpath)
         
@@ -99,7 +115,11 @@ if __name__=="__main__":
             if not line=='':
                 content.append(line)
         
-        start_index = content.index('<option value="cost_change_event_fall">Price fall (round)</option>')+1
+        try:
+            start_index = content.index('<option value="cost_change_event_fall">Price fall (round)</option>')+1
+        except:
+            print ef, sf, page
+            
         index = start_index
         isHeader = True
         while not content[index]=='    </table>':
@@ -109,28 +129,25 @@ if __name__=="__main__":
                     index += 1
                 block = content[block_start+1:index]
                 if isHeader:
-                    headerContent = ParseContentBlock(block)
+                    _i, headerContent = ParseContentBlock(block)
                     isHeader = False
-#                     print headerContent
                 else:
-                    blockContent = ParseContentBlock(block)
-                    pass
-                    
-#                     Database.add_data_block(headerContent, blockContent)
+                    playerID, blockContent = ParseContentBlock(block)
+                    Database.add_data_block(playerID, headerContent, blockContent)
                     
             else:
                 index += 1
             
-            
-        
-        print page, len(content), start_index, index
+    serial.save('Database.pkl', Database)
     
-    print Database.data
+    print "Total time: ", time.time() - t
     
+    """
     
-    
-    
-    
+    Database = serial.load('Database.pkl')
+    print len(Database.data.keys())
+    for key in Database.data[1]:
+        print key
     
     
     
